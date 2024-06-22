5819418fbeed4ea012fda4117f646ef8bd12b977
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-06-22 18:05:01 +0200

    clear

--- src/com.academiews/Car.java
+++ src/com.academiews/Car.java
@@ -3,7 +3,11 @@ package com.academiews;
 public class Car { // pascal naming convention
     public String model; // fields // properties
 
-    public void setModel(String model) {
+    public void setModel(String model) { // setter
         this.model = model;
+    }
+
+    public void clear() {
+        this.model = "";
     }
 }
