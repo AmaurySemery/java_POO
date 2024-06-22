1c976196051b3a3f31fbdd02ac8cf18a04f146bb
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-06-22 17:51:06 +0200

    first commit

---
+++ src/com.academiews/Car.java
@@ -0,0 +1,9 @@
+package com.academiews;
+
+public class Car { // pascal naming convention
+    public String model; // fields // properties
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
