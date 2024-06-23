49fea5771de80c8607c5da596fa1b4f358124228
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-06-23 21:48:40 +0200

    Héritage

---
+++ src/com.academiews/Accountant.java
@@ -0,0 +1,15 @@
+package com.academiews;
+
+public class Accountant extends Employee {
+
+    public Accountant(int baseSalary) {
+        super(baseSalary);
+        //TODO Auto-generated constructor stub
+    }
+
+    public Accountant(int baseSalary, int hourlyRate) {
+        super(baseSalary, hourlyRate);
+        //TODO Auto-generated constructor stub
+    }
+
+}
